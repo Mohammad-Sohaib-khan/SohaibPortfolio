820bf5dfeafc90551ad8708085ca6096cf77f0ce
Mohammad-Sohaib-khan <122706877+Mohammad-Sohaib-khan@users.noreply.github.com>
2023-08-03 21:56:56 -0400

    Add files via upload

---
+++ backend/dist/static/index-684ed8dd-341fb6d1.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-e98b3e3a-f62c6d25.js";import"./sanity-448e7147.js";import"./json-inspector-d8d7bb53.js";export{m as DocumentPaneProvider,a as default};
---
+++ backend/schemas/index.ts
@@ -0,0 +1,19 @@
+
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import about from './about'
+import experience from './experience'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [testimonials,works,brands,about,
+                           skills,workExperience,experience,contact
+                           ]
+
+
+
+
+
+                           
